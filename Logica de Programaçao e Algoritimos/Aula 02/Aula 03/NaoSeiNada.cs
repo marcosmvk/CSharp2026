a8af40faa22850299bf413d7eaef818bf0d5c8f8
marcosmvk <marcosalmeida1778@gmail.com>
2026-02-13 19:10:18 -0300

    aula03

---
+++ Logica de Programaçao e Algoritimos/Aula 02/Aula 03/NaoSeiNada.cs
@@ -0,0 +1,7 @@
+double x, y, media;
+Console.Writeline ("Digite o primeiro numero: ");
+x=double.Parse(Console.ReadLine());
+Console.Writeline("Digite o segundo numero: ");
+y=double.Parse(Console.ReadLine());
+media=(x+y)/2.0;
+Console.Writeline("Media ="+ media);
