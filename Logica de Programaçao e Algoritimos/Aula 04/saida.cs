206d1811643d36380d203b8f48bdf6da4a346f57
marcosmvk <marcosalmeida1778@gmail.com>
2026-02-18 19:43:28 -0300

    aula 04

---
+++ Logica de Programaçao e Algoritimos/Aula 04/saida.cs
@@ -0,0 +1,17 @@
+//Tipos de variaveis e saida de dados
+string nome;//Criado a variavel
+nome = "Senai Americana";//Dados do tipo String
+Console.WriteLine(nome);//Saida de dados//
+Console.Write(nome);//Com quebra de linha
+Console.Write(nome);//Sem quebra de linha
+Console.WriteLine(nome);
+System.Console.WriteLine(nome);//Com snippets
+string pessoa;
+pessoa = "Clodoaldo";
+Console.WriteLine(pessoa);
+const string ESCOLA = "Senai Americana 1";//Constante
+System.Console.WriteLine(ESCOLA);
+Console.WriteLine("O nome do usuário é:"+ pessoa);//Concatenacao
+System.Console.WriteLine("C"+"e"+"s"+"a"+"r");
+//Interpolação
+System.Console.WriteLine($"O nome da escola é {ESCOLA}");//Interpolação
