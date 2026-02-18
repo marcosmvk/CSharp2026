befa6e3f1ba53d0007764cfac643a6499fcd5e1f
marcosmvk <marcosalmeida1778@gmail.com>
2026-02-18 20:44:11 -0300

    aula4

--- Logica de Programaçao e Algoritimos/Aula 04/processamento.cs
+++ Logica de Programaçao e Algoritimos/Aula 04/processamento.cs
@@ -7,4 +7,4 @@ WriteLine(2*3);//Multiplicação
 WriteLine(6/3);//Divisão
 WriteLine(7%3);//Resto da divisão
 WriteLine(2*2);//Potenciação
-WriteLine(2 + 2 / 2);
+WriteLine((2+2) / (2+2));//Ordem de operação
