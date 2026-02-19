5721d1162fc8e4b47670793294d1a439806803bb
marcosmvk <marcosalmeida1778@gmail.com>
2026-02-18 21:05:48 -0300

    aula 4

---
+++ Logica de Programaçao e Algoritimos/Aula 04/entrada.cs
@@ -0,0 +1,10 @@
+//Entrada de dados
+Console.WriteLine("Digite o seu nome: ");
+string nome = Console.ReadLine();//Entrada de dados
+System.Console.WriteLine($"O valor digitado é de {nome}");
+System.Console.WriteLine("Digite o 1° numero: ");
+double x = double.Parse(Console.ReadLine());
+System.Console.WriteLine("Digite o 2° numero: ");
+double y = double.Parse(Console.ReadLine());
+double soma = x+y;
+System.Console.WriteLine($"A soma dos dois valores é de {soma}");
