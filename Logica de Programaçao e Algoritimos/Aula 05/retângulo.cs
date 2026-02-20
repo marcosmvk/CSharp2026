8c3e8730c8975afb9123508f89b29ea36eca8602
marcosmvk <marcosalmeida1778@gmail.com>
2026-02-20 19:43:04 -0300

    aula 05

---
+++ Logica de Programaçao e Algoritimos/Aula 05/retângulo.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.Design;
+using System.Net;
+using static System.Console;
+//Declaração de variaveis
+double bas, altura, area, perimetro, diagonal;
+//Entrada de dados 
+Write("Digite a base do retangulo: ");
+bas = double.Parse(ReadLine());
+Write("Digite a altura do retangulo: ");
+altura = double.Parse(ReadLine());
+Write("Digite o perimetro do retangulo: ");
+perimetro = double.Parse(ReadLine());
+//Processamento de dados 
+area = bas * altura;
+perimetro = 2 * (bas + altura);
+diagonal = Math.Sqrt((bas * bas) + (altura * altura));
+//Saida de dados 
+WriteLine($"O valor da area é {area}, perimetro {perimetro} e diagonal  {diagonal} deste retangulo:");
+
+
+
+
