669a03d572e41e99b5466005e6f4ccaf6594e1c2
marcosmvk <marcosalmeida1778@gmail.com>
2026-02-23 18:31:52 -0300

    aula 05

--- troco.cs
+++ troco.cs
@@ -1,3 +1,14 @@
 using static System.Console;
-//Declaração de variaveis
-double valor1, valor2, 
+Console.Write("Preço unitário:");
+double preco = double.Parse(ReadLine());
+Console.Write("Quantidade:");
+int quantidade = int.Parse(ReadLine());
+Console.Write("Dinheiro recebido:");
+double dinheiro = double.Parse(ReadLine());
+double troco = dinheiro - (preco * quantidade);
+Console.WriteLine($"Troco: {troco}");
+
+
+
+
+
