bd638c74e38bc1a7af0b41d48f4d49b2d4ce984f
marcosmvk <marcosalmeida1778@gmail.com>
2026-02-23 19:30:39 -0300

    Com Try e Catch

--- Logica de Programaçao e Algoritimos/Aula 06/medidas.cs
+++ Logica de Programaçao e Algoritimos/Aula 06/medidas.cs
@@ -1,20 +1,26 @@
 
 using static System.Console;
-double a, b, c, areaQuadrado, areaTriangulo, areaTrapezio;
-Write("Digite a medida A:");
-a = double.Parse(ReadLine());
-Write("Digite a medida B:");
-b = double.Parse(ReadLine());
-Write("Digite a medida C:");
-c = double.Parse(ReadLine());
-// Processamento de dados
-areaQuadrado = a * a;
-areaTriangulo = (a * b)/2;
-areaTrapezio = ((a + b) * c) /2;
-//Saida de dados
-WriteLine($"Area do Quadrado = {areaQuadrado:F4}");
-WriteLine($"Area do Triangulo = {areaTriangulo:F4}");
-WriteLine($"Area do Trapezio = {areaTrapezio:F4}");
+try{
+    //Declaração de variaveis
+    double a, b, c, areaQuadrado, areaTriangulo, areaTrapezio;
+    Write("Digite a medida A:");
+    a = double.Parse(ReadLine());
+    Write("Digite a medida B:");
+    b = double.Parse(ReadLine());
+    Write("Digite a medida C:");
+    c = double.Parse(ReadLine());
+    // Processamento de dados
+    areaQuadrado = a * a;
+    areaTriangulo = (a * b)/2;
+    areaTrapezio = ((a + b) * c) /2;
+    //Saida de dados
+    WriteLine($"Area do Quadrado = {areaQuadrado:F4}");
+    WriteLine($"Area do Triangulo = {areaTriangulo:F4}");
+    WriteLine($"Area do Trapezio = {areaTrapezio:F4}");
+}catch (Exception)
+{
+    WriteLine("Valores invalidos inseridos, programa encerrado");
+}
 
 
 
