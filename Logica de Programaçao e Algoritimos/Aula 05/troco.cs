8c933fb663026c944652f9f34c70664bfd6e674c
marcosmvk <marcosalmeida1778@gmail.com>
2026-02-25 18:35:51 -0300

    .

--- Logica de Programaçao e Algoritimos/Aula 05/troco.cs
+++ Logica de Programaçao e Algoritimos/Aula 05/troco.cs
@@ -1,10 +1,12 @@
 using static System.Console;
+double preco, dinheiro;
+int quantidade;
 Console.Write("Preço unitário:");
-double preco = double.Parse(ReadLine());
+preco = double.Parse(ReadLine());
 Console.Write("Quantidade:");
-int quantidade = int.Parse(ReadLine());
+quantidade = int.Parse(ReadLine());
 Console.Write("Dinheiro recebido:");
-double dinheiro = double.Parse(ReadLine());
+dinheiro = double.Parse(ReadLine());
 double troco = dinheiro - (preco * quantidade);
 Console.WriteLine($"Troco: {troco}");
 
