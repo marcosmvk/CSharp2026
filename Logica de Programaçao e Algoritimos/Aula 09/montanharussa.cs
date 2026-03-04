6ad50ebd82574f67d833811f0a34df12e035e65c
marcosmvk <marcosalmeida1778@gmail.com>
2026-03-04 18:34:00 -0300

    Aula 09

--- Logica de Programaçao e Algoritimos/Aula 09/montanharussa.cs
+++ Logica de Programaçao e Algoritimos/Aula 09/montanharussa.cs
@@ -1,18 +1,18 @@
 using System.Reflection;
 using static System.Console;
 // Declaração de variaveis 
-double contagem = 0, alturaMaxima, alturaMinima, pessoas;
+double contagem = 0, alturaMaxima, alturaMinima, visitantes;
 try
 {
-    WriteLine("Digite o número de alturas das pessoas: ");
-    pessoas = double.Parse(ReadLine());
+    WriteLine("Digite o número de visitantes: ");
+    visitantes = double.Parse(ReadLine());
     WriteLine("Digite a altura mínima (cm): ");
     alturaMinima = double.Parse(ReadLine());
     WriteLine("Digite a altura máxima (cm): ");
     alturaMaxima = double.Parse(ReadLine());
-   for (double pessoa = 0; pessoa < pessoas; pessoa++)
+   for (double pessoa = 0; pessoa < visitantes; pessoa++)
     {
-        WriteLine($"Digite a altura da pessoa {pessoa +1}:");
+        WriteLine($"Digite a altura do visitante {pessoa +1}:");
         double alturaPessoa = double.Parse(ReadLine());
         if (alturaPessoa >= alturaMinima && alturaPessoa <= alturaMaxima)
         {
