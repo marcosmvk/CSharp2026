af2cfad2c266d0dd88059214663d9df11a1943d3
marcosmvk <marcosalmeida1778@gmail.com>
2026-03-04 19:49:05 -0300

    Aula 03

--- Logica de Programaçao e Algoritimos/Aula 10/senha.cs
+++ Logica de Programaçao e Algoritimos/Aula 10/senha.cs
@@ -1,17 +1,30 @@
 using static System.Console;
 //Declaração de variaveis 
 const int SENHAFIXA = 2002;
-int senha;
+int senha, contagem = 0;
 //Entrada de dados
 Write("Digite a senha: ");
 senha = int.Parse(ReadLine());
 //Processamento de dados
 while (senha != SENHAFIXA)
 {
-    Write("Senha incorreta, digite novamente");
+    if (contagem >= 3)
+    {
+        WriteLine("Acesso bloqueado!");
+        break;
+    }
+    contagem++;
+    Write("Senha incorreta, digite novamente: ");
     senha = int.Parse(ReadLine());
 }
-WriteLine("Acesso permitido!");
+if (contagem < 3)
+{
+    WriteLine("Acesso permitido!");
+}
+else
+{
+    Write("Acesso bloqueado, aguarde 30 segundos");
+}
 
 
 
