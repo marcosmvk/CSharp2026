5994370193b19804f5c309a42d4eed5f0ef3f9b6
marcosmvk <marcosalmeida1778@gmail.com>
2026-03-09 18:38:41 -0300

    Aula 10

--- Logica de Programaçao e Algoritimos/Aula 10/loopWhile.cs
+++ Logica de Programaçao e Algoritimos/Aula 10/loopWhile.cs
@@ -1,30 +1,34 @@
 using static System.Console;
 //Declaração de variaveis
 int turistas = 0, quantidade, contagem = 0;
-string entrada, saida, sair;
+string entrada;
 //Entrada de dados
 WriteLine("Bem-vindo ao controle de fluxo de turistas no parque!");
+while(true)
+{
 Write("Digite o fluxo de carro (entrada/saida) ou 'sair' para encerrar: ");
 entrada = ReadLine(); 
-Write("Digite o numero de turistas: ");
-quantidade = int.Parse(ReadLine());
-while(true)
-if (entrada == "entrada")
-{
-    turistas += quantidade;
-}
-else if (entrada == "saida")
-{
-    turistas -= quantidade;
-}
-else if (entrada == "sair")
+if (entrada == "sair")
 {
     WriteLine("Programa encerrado");
    break;
 }
+Write("Digite o numero de turistas: ");
+quantidade = int.Parse(ReadLine());
+if (entrada == "entrada")
+{
+    turistas += quantidade;
+    contagem++;
+}
+else if (entrada == "saida")
+{
+    turistas -= quantidade;
+    contagem--;
+}
 else
 {
     WriteLine("Entrada inválida, tente novamente.");
+}
 }
 WriteLine($"Total de turistas no parque: {turistas}");
 WriteLine($"Contagem de entradas e saídas: {contagem}");
