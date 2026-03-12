f15593c66a50cb1ff5119f9fdd67b48b50594071
marcosmvk <marcosalmeida1778@gmail.com>
2026-03-11 21:50:10 -0300

    Aula 12

--- Logica de Programaçao e Algoritimos/Aula 12/tupla.cs
+++ Logica de Programaçao e Algoritimos/Aula 12/tupla.cs
@@ -9,4 +9,5 @@ System.Console.WriteLine($"Nome do filho do clodo: {clodoaldo.Item3}");
 clodoaldo.Item1 = 54;
 System.Console.WriteLine($"Idade do clodoaldo: {clodoaldo.Item1}");
 
-
+(int, int, int) numeros = (1, 2, 5);
+System.Console.WriteLine($"Números: {numeros.Item1}, {numeros.Item2}, {numeros.Item3}");
