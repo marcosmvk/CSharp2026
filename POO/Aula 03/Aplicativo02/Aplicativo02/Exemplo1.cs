fd35317f6590612f0a41be4e49c698f0f1f3d6a9
marcosmvk <marcosalmeida1778@gmail.com>
2026-03-20 20:57:22 -0300

    Aula 03

---
+++ POO/Aula 03/Aplicativo02/Aplicativo02/Exemplo1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aplicativo02
+{
+    internal class Funcionario
+    {
+        //Campos da classe
+        public string nome;
+        public double salariobruto;
+        public double imposto;
+
+        //Construtor 
+        public Funcionario(string nome, double salariobruto, double imposto)
+        {
+            this.nome = nome;
+            this.salariobruto = salariobruto;
+            this.imposto = imposto;
+        }
+
+        //Metodos 
+        public double SalarioLiquido()
+        {
+            return salariobruto -= imposto;
+        }
+
+        public void AdicionarAumento(double porcentagem)
+        {
+            salariobruto = salariobruto * porcentagem
+     
+           
+        }
+    
+        public string Dados_Do_Funcionario()
+        {
+            return $"Nome{nome} Salario Bruto: {salariobruto} Imposto: {imposto}" +
+                $"Salario Liquido: {SalarioLiquido()}";
+
+        }
+        
+        public string Salario_Atualizado()
+
+        {
+            
+            return $"Nome{nome} Salario liquiddo: {SalarioLiquido} ";
+
+        }
+    }
+}
+
---
+++ POO/Aula 03/Aplicativo02/Aplicativo02/Program.cs
@@ -0,0 +1,33 @@
+﻿using Aplicativo02;
+using static System.Console;
+
+
+WriteLine("Entre com os dados do funcionario: ");
+Write("Nome: ");
+string nome = ReadLine();
+Write("Salário Bruto: ");
+double salariobruto = double.Parse(ReadLine());
+Write("Imposto: ");
+double imposto = double.Parse(ReadLine());
+
+Funcionario f = new Funcionario(nome,salariobruto, imposto);
+
+WriteLine("Dados do Funcionario");
+WriteLine($"Nome do Funcionario: {nome}");
+WriteLine($"Salario Liquido: {f.SalarioLiquido()}");
+
+Write("Digite a porcentagem de aumento do salario do funcionario:");
+double porcentagem = double.Parse(ReadLine());
+f.AdicionarAumento(porcentagem);
+WriteLine($"Salario do funcionario aumentado em: {porcentagem}%");
+
+WriteLine("Dados do funcinário atualizado");
+WriteLine($"Nome do Funcionario: {nome}");
+WriteLine($"Salario Liquido: {f.SalarioLiquido()}");
+
+
+
+
+
+
+ReadKey();
