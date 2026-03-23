476577f50725d69901f2eb5cef1df42ee9f38db3
marcosmvk <marcosalmeida1778@gmail.com>
2026-03-23 20:05:38 -0300

    Aula 04

---
+++ POO/Aula 04/Aula 04/Aula 04/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+using static System.ConsoleColor;
+
+Title = "Calculadora 01";
+
+//Declaração de variaveis 
+double raio;
+const double PI = 3.14;
+
+//Entrada de dados 
+Write("Entre com o valor do raio da circunferencia: ");
+ForegroundColor = Red;
+raio = double.Parse(ReadLine());
+ResetColor();
+
+//Saida de dados 
+WriteLine($"Circunferencia: {Circunferencia(raio):F2}");
+WriteLine($"Volume: {Volume(raio):F2}");
+WriteLine($"PI: {PI}");
+
+//Calculo da circunferencia 
+
+double Circunferencia(double r)
+{
+    return 2 * PI * raio;
+}
+
+double Volume (double r)
+{
+    return (4 / 3) * PI * r * r * r;
+}
+
+
+ReadKey();
