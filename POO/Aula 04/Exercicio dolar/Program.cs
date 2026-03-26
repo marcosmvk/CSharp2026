aa743470790e80dd9c5b414f6e3835013681633e
marcosmvk <marcosalmeida1778@gmail.com>
2026-03-25 21:49:44 -0300

    Aula 05

--- POO/Aula 04/Exercicio dolar/Program.cs
+++ POO/Aula 04/Exercicio dolar/Program.cs
@@ -3,13 +3,14 @@ using static System.Console;
 
 double cotacao, qtde;
 
+//Entrada de dados
 Write("Qual é cotação do dolar ");
 cotacao = double.Parse(Console.ReadLine());
 Write("Quantos dolares você vai comprar? ");
 qtde = double.Parse(Console.ReadLine());
 
 double total = ConversorDeMoeda.Converter(cotacao,qtde);
-
+//Saida de dados 
 WriteLine($"Total a pagar em reais:{total}");
 
 ReadKey();
---
+++ POO/Aula 05/Banco/Banco/Program.cs
@@ -0,0 +1,47 @@
+﻿using static System.Console;
+using Banco;
+using static System.Globalization.CultureInfo;
+
+
+
+Title = "Abertura de conta";
+ContaBancaria conta;
+
+WriteLine("Abertura de conta - Nika Investiment");
+Write("Entre com um número de conta: ");
+int numero = int.Parse(Console.ReadLine());
+Write("Entre com o nome do titular: ");
+string nome = Console.ReadLine();
+Write("Deseja fazer deósito inicial (s/n): ");
+char resposta = char.Parse(Console.ReadLine().ToLower());
+
+if (resposta == 's')
+{
+    Write("Entre com o valor de depósito inicial: ");
+    double deposito = double.Parse(ReadLine());
+     conta = new ContaBancaria(numero, nome, deposito);
+
+}
+
+else
+{
+     conta = new ContaBancaria(numero, nome);
+}
+
+
+conta.Dados();
+
+WriteLine("Entre com um valor para depósito: R$ ");
+double quantia = double.Parse(Console.ReadLine());      
+conta.Deposito(quantia);
+conta.Dados();
+WriteLine("Entre com um valor para saque: R$ ");
+quantia = double.Parse(Console.ReadLine());
+conta.Saque(quantia);
+conta.Dados();
+
+
+
+
+
+ReadKey();
