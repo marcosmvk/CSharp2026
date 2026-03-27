08464104f5eac0cae0bdc2dcbcb6cacd67542d4e
marcosmvk <marcosalmeida1778@gmail.com>
2026-03-27 20:57:29 -0300

    Aula 06

---
+++ POO/Aula 06/Banco1/Banco1/ContaBancaria.cs
@@ -0,0 +1,72 @@
+﻿
+
+
+namespace Banco1
+{
+    internal class ContaBancaria
+    {
+		//Campo 
+		private int numero;
+		private string titular;
+		private double saldo;
+
+		//Propriedades
+
+		public int Numero
+		{
+			get { return numero; }
+			set { numero = value; }
+		}
+		public string Titular
+		{
+			get { return titular; }
+			set { titular = value; }
+		}
+        public double Saldo
+        {
+            get { return saldo; }
+            set 
+			{ 
+				if (value > 0) 
+				{ saldo = value; } 
+				else { saldo = 0; }
+			   
+			}
+        }
+
+		//Construtor
+
+		public ContaBancaria(int  numero, string titular)
+		{
+			Numero = numero;
+			Titular = titular;
+			Saldo = 0;
+		}
+
+		public ContaBancaria(int numero, string titular, double saldo):this(numero, titular)
+        {
+			Saldo = saldo;
+		}
+
+		//Métodos 
+		
+		public void Deposito(double quantia)
+		{
+			saldo += quantia;
+		}
+
+		public void Saque(double quantia)
+		{
+			saldo -= quantia; 
+		}
+
+		public void Dados()
+		{
+			Console.WriteLine($"Dados da conta: \n " + 
+				$"\tConta: {numero}\n" +
+				$"\tTitular: { Titular}\n" +
+				$"\tSaldo: {Saldo:C}");
+		}
+
+    }
+}
---
+++ POO/Aula 06/Banco1/Banco1/Program.cs
@@ -0,0 +1,41 @@
+﻿using Banco1;
+using static System.Console;
+
+Title = "Cadastro de conta";
+ContaBancaria conta;
+
+//Entrada de dados 
+Write("Entre com o número da conta: ");
+int n = int.Parse(Console.ReadLine());
+Write("Entre com o nome do titutalr da conta: ");
+string nome = ReadLine();
+Write("Deseja fazer depósito inicial (s/n): ");
+char resposta = char.Parse(ReadLine().ToLower());   
+
+if (resposta == 's')
+{
+    Write("Entre com o valor do depósito inicial: ");
+    double dep = double.Parse(ReadLine());
+    conta = new ContaBancaria(n, nome, dep);
+}
+else
+{
+    conta = new(n, nome);
+}
+
+conta.Dados();
+
+
+Write("Deseja fazer saque (s/n): ");
+resposta = char.Parse(ReadLine().ToLower());
+
+if (resposta == 's')
+{
+    Write("Entre com o valor do saque: ");
+    double saque = double.Parse(ReadLine());
+    conta.Saque(saque);
+
+}
+
+ReadKey();
+
