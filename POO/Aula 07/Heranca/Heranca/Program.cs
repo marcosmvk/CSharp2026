cc144c92103bd991497b2fa79d2cdc04ae401365
marcosmvk <marcosalmeida1778@gmail.com>
2026-03-30 21:01:24 -0300

    Aula 07

---
+++ POO/Aula 07/Heranca/Heranca/Program.cs
@@ -0,0 +1,7 @@
+﻿using Heranca;
+
+PessoaFisica cesar = new PessoaFisica(12345, "César");
+PessoaJuridica senai = new PessoaJuridica(54321, "Skaf", 12000.00);
+
+Console.WriteLine (cesar); //Imprimir somente o objt
+Console.WriteLine(cesar.ToString());//Imprimi e converte o obj p/ string
