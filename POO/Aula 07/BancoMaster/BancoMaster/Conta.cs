cbb71ee58ff0f67a1ce0e7498e3191f649438230
marcosmvk <marcosalmeida1778@gmail.com>
2026-03-30 21:54:36 -0300

    Aula 07 BancoMaster

---
+++ POO/Aula 07/BancoMaster/BancoMaster/Conta.cs
@@ -0,0 +1,53 @@
+﻿
+
+namespace BancoMaster
+{
+    internal class Conta
+    {
+		//Campo
+		private int numero;
+		private string titular;
+		private double saldo;
+
+        //Propriedades
+        public int NumeroConta
+		{
+			get { return numero; }
+			set { numero = value; }
+		}
+		public string TitularConta
+		{
+			get { return titular; }
+			set { titular = value; }
+		}
+		public double SaldoConta
+		{
+			get { return saldo; }
+			set { saldo = value; }
+		}
+
+		//Construtores
+        public Conta(int numeroConta, string titularConta, double saldoConta)
+        {
+            NumeroConta = numeroConta;
+            TitularConta = titularConta;
+            SaldoConta = saldoConta;
+        }
+
+        public Conta(int numeroConta, string titularConta):this(numeroConta, titularConta, 0) 
+        {
+            SaldoConta = 0;
+        }
+
+		//Métodos
+		public void Saque(double quantia)
+		{
+			SaldoConta -= quantia + 5;
+		}
+
+		public void Deposito(double quantia)
+		{
+			SaldoConta += quantia;
+		}
+    }
+}
---
+++ POO/Aula 07/BancoMaster/BancoMaster/ContaPoupanca.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace BancoMaster
+{
+    internal class ContaPoupanca : Conta
+    {
+        //Campos 
+        private double taxa;
+
+        //Propriedades
+        public double taxaJuros
+        {
+            get { return taxa; }
+            set { taxa = 0; }
+        }
+
+        //Construtores
+        public ContaPoupanca(int numeroConta, string titularConta, double taxaJuros) : base(numeroConta, titularConta)
+        {
+            taxa = 0;
+        }
+
+        public ContaPoupanca(int numeroConta, string titularConta, double saldoConta, double taxaJuros) : base(numeroConta, titularConta, saldoConta)
+        {
+            taxa = 0;
+        }
+
+        //Métodos 
+        public void Saque(double quantia)
+        {
+            SaldoConta += quantia;
+        }
+            
+    }
+}
